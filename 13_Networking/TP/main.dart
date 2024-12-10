d492b23fc9e87fbfe51d4b88a0c0839a2dde850c
zivana afra yulianto <afrazivana5@gmail.com>
2024-12-11 04:47:30 +0700

    12_maps_&_Place

---
+++ 13_Networking/TP/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+/// Controller untuk mengelola state counter
+class CounterController extends GetxController {
+  // Variabel untuk menyimpan nilai counter
+  var counter = 0.obs;
+
+  // Fungsi untuk menambah nilai counter
+
+  void increment() {
+    counter++;
+  }
+
+  // Fungsi untuk mereset nilai counter
+  void reset() {
+    counter.value = 0;
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final CounterController controller = Get.put(CounterController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Counter App")),
+      body: Center(
+        child: Obx(() {
+          // Menampilkan nilai counter
+          return Text(
+            "${controller.counter}",
+            style: TextStyle(fontSize: 48),
+          );
+        }),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              // Logika untuk menambah nilai counter
+              controller.increment();
+            },
+            child: Icon(Icons.add),
+          ),
+          SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: () {
+              // Logika untuk mereset nilai counter
+              controller.reset();
+            },
+            child: Icon(Icons.refresh),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
